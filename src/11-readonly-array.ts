b0ee0bfda53ff4feff16b65e37aba69e5423bba0
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-06-14 12:30:01 -0500

    functioning of ReadonlyArray in TypeScript.

---
+++ src/11-readonly-array.ts
@@ -0,0 +1,9 @@
+const numbers: ReadonlyArray<number> = [1, 2, 3, 4];/*
+numbers.push(1);
+numbers.pop();
+numbers.unshift(1);
+//mutability
+*/
+numbers.filter(()=>{});
+numbers.reduce(()=>0);
+numbers.map(()=>0);
